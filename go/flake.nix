edabf7c23ebd348a871b1c124544c96aab2b8020
kytnacode <kytnacode@gmail.com>
2026-06-10 12:55:35 -0700

    Remove package declaration from go module

--- go/flake.nix
+++ go/flake.nix
@@ -5,10 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
+  outputs = {nixpkgs, ...}: let
     systems = ["x86_64-linux"];
     eachSystem = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs {inherit system;}));
   in {
@@ -24,21 +21,6 @@
 
         CGO_ENABLE = 0;
       };
-    });
-
-    packages = eachSystem (pkgs: rec {
-      default = __PACKAGE_NAME__;
-
-      __PACKAGE_NAME__ = pkgs.buildGoModule (finalAttrs: {
-        pname = "__BIN_NAME__";
-        version = "0.1.0";
-
-        src = ./.;
-
-        vendorHash = "__PACKAGE_HASH__";
-
-        env.CGO_ENABLED = 0;
-      });
     });
   };
 }
