ba1265d6f8dc19cddf98e989bb27148e2e8e22bf
kytnacode <kytnacode@gmail.com>
2026-06-25 00:38:00 -0700

    Remove unnecesary self

--- flake.nix
+++ flake.nix
@@ -5,10 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
+  outputs = {nixpkgs, ...}: let
     systems = ["x86_64-linux"];
     eachSystem = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs {inherit system;}));
   in {
